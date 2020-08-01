f1788c832851ebda4c218fd696ca816e641a7160
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-01 13:36:47 -0600

    Add bcrypt to encrypt password

---
+++ app.js
@@ -0,0 +1,8 @@
+const unsecurePlainTextPassword = 'TestPassword';
+const bcrypt = require('bcrypt');
+
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
+        console.log(hash);
+    });
+});
