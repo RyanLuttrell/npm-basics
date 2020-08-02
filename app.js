65db42954e5dcd6bce0003b227d23ac6008e8e14
RyanLuttrell <ryan.m.luttrell@gmail.com>
2020-08-01 20:41:27 -0600

    adding gitignore  and node_modules

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const unsecurePlainTextPassword = 'TestPassword';
 const bcrypt = require('bcrypt');
+const colors = require('colors');
 
 bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
-        console.log(hash);
+        console.log(hash.blue);
     });
 }); 
